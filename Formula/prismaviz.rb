726eefec534acba36a73db152672e6ab5f9a0f23
Viijay-Kr <vjkrishna689@gmail.com>
2023-09-07 17:59:45 +0200

    update formula

--- Formula/prismaviz.rb
+++ Formula/prismaviz.rb
@@ -1,14 +1,14 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Prsv < Formula
+class Prismaviz < Formula
   desc "Visualise Prisma schema in your terminal"
   homepage "https://github.com/Viijay-Kr/prisma-visualise"
-  url "https://github.com/Viijay-Kr/prisma-visualise/releases/download/v0.0.1/prsv-mac.tar.gz"
-  sha256 "591e9a84e83209706f23a5377a9200eebb16b39d21b6664f47db56012208afc1"
+  url "https://github.com/Viijay-Kr/prisma-visualise/releases/download/v0.0.1/prismaviz-mac.tar.gz"
+  sha256 "9afb2dd2fd99a9297504cce3d7a4c2c77c2bd22293538bd16ba8d59792621069"
   version "0.0.1"
 
   def install
-    bin.install "prsv"
+    bin.install "prismaviz"
   end
 end
