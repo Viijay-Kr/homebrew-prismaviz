50724fb058f60061a9ac150645f1d9abf8a4fbe0
Viijay-Kr <vjkrishna689@gmail.com>
2023-09-07 17:08:06 +0200

    updated binary name

--- Formula/prsv.rb
+++ Formula/prsv.rb
@@ -5,7 +5,7 @@ class Prsv < Formula
   desc "Visualise Prisma schema in your terminal"
   homepage "https://github.com/Viijay-Kr/prisma-visualise"
   url "https://github.com/Viijay-Kr/prisma-visualise/releases/download/v0.0.1/prsv-mac.tar.gz"
-  sha256 "531c48d1b5ca0daaae0f62aededc91d95848df1dd074c49e32f61bf42e6b8cf4"
+  sha256 "591e9a84e83209706f23a5377a9200eebb16b39d21b6664f47db56012208afc1"
   version "0.0.1"
 
   def install
